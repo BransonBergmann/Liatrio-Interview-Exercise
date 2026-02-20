13450010bf04f07ba7bd2d48294b0055ac1f22bc
BransonBerg <bcbergmann@csuchico.edu>
2026-02-20 15:54:52 -0800

    starting a build of the foundational send w/ both the proper message & timestamp, not currently outputting properly

--- send_message.go
+++ send_message.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"log"
+	"time"
 
-	
 	"github.com/gofiber/fiber/v2"
 )
+
+type output struct {
+	message		string
+	timestamp 	int64
+}
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/text", func(c *fiber.Ctx) error {
-		return c.SendString("My name is Branson Bergmann!")
+	output := output {
+		message: "My name is Branson Bergmann",
+		timestamp: time.Now().Unix(),
+	}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(output)
 	})
 
 	log.Fatal(app.Listen(":8080"))
